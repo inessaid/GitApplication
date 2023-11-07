c146a88ca867cd8cb0d785b6d55dd5f7731846e3
iness <inessaid88@gmail.com>
2023-11-06 23:05:33 -0500

    changed int

--- app/src/main/java/com/example/gitapplication/MainActivity.java
+++ app/src/main/java/com/example/gitapplication/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        num =5;
+        num =4;
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
